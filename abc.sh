8f965a5366813149815639c27afceba7e3dbfc33
syamsss6 <syamsss6@gmail.com>
2016-05-23 22:20:14 +0530

    print

---
+++ abc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+a=0
+b=0
+
+echo -n "Enter the first number --> "
+read a
+echo -n "Enter the second number -> "
+read b
+
+
+for ((i=a;i<=b;i++));
+do
+  echo $i
+done
+~            
